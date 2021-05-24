3d4c8dba1c0c1073b4b669fff79e41c702b28e86
rafiq <rafiq.i@live.co.uk>
2021-05-24 09:33:30 +0600

    making stan clien for base publishr protected from private

--- src/events/ticket-updated-event.ts
+++ src/events/ticket-updated-event.ts
@@ -8,5 +8,10 @@ export interface TicketUpdatedEvent {
     title: string;
     price: number;
     userId: string;
+    //ticket created event dose not have orderId, but ticket updated event has
+    //because an ordered ticket could be canceled, then we need to update the ticket
+    //by incrementing verson and orderId to null/undefined, since version chnaged every
+    //service who listen order updated need to know ticket property changed
+    orderId: string;
   };
 }
